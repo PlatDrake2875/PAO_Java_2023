acea05add1d06a26e4d90fdbf0d2ddb859068b3b
Adrian Patrascu <adrian-octavian.patrascu@s.unibuc.ro>
2023-04-01 15:34:54 +0300

    Added setters and getters to Song class

--- Tema1/src/main/java/ro/pao/model/audioContents/Podcast.java
+++ Tema1/src/main/java/ro/pao/model/audioContents/Podcast.java
@@ -1,4 +1,25 @@
 package ro.pao.model.audioContents;
 
-public class Podcast {
+import ro.pao.model.abstracts.AbstractAudioContent;
+import ro.pao.model.enums.AudioContentType;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+public class Podcast extends AbstractAudioContent {
+    private String genre;
+    private String[] guests;
+    private String[] topics;
+
+    protected Podcast(String title, Date year, LocalTime duration, AudioContentType type, String genre, String[] guests, String[] topics) {
+        super(title, year, duration, type);
+        this.genre = genre;
+        this.guests = guests;
+        this.topics = topics;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing podcast");
+    }
 }
